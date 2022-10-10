054e78a50eead331923459ac268bc3d1dd52e6ed
glorieya <110552604+glorieya@users.noreply.github.com>
2022-10-10 15:33:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,30 @@
 from typing import List
 
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  # Write code here
+def partition(arr, l, r):
+    #Function implementing partitioning of sub-array arr[l:r]
+    #Return: -> index of pivot
+    p, curr = l, l + 1
+    while curr <= r:
+        if arr[curr] <= arr[l]:
+            p += 1
+            arr[p], arr[curr] = arr[curr], arr[p]
+        curr += 1
+    arr[p], arr[l] = arr[l], arr[p]
+    return p
 
+def quick_sort(data, l, r) -> List[int]:
+     #Function implementing in-place quick sort of data[l:r]
+    if l < r:
+        p = partition(data, l, r)
+        quick_sort(data, l, p - 1)
+        quick_sort(data, p + 1, r)
+    return data
 
-# Do not change the following code
-nums1 = []
-nums2 = []
-for item in input().split(', '):
-  nums1.append(int(item))
-for item in input().split(', '):
-  nums2.append(int(item))
-m = int(input())
-n = int(input())
-merge(nums1, m, nums2, n)
-print(nums1)
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(quick_sort(data, 0, len(data)-1))
